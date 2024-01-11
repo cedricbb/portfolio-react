9a50c8b93fd01ac8b347581909e0c1d9ff0dea40
cbillard <cbillard@com-company.fr>
2024-01-11 15:56:09 +0100

    Services page finished

---
+++ constants/Services.js
@@ -0,0 +1,44 @@
+// icons
+import {
+    RxCrop,
+    RxPencil2,
+    RxDesktop,
+    RxReader,
+    RxRocket,
+    RxArrowTopRight
+  } from "react-icons/rx";
+  
+  
+  // data
+  export const serviceData = [
+    {
+      icon: <RxCrop />,
+      title: 'Branding',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: <RxPencil2 />,
+      title: 'Design',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: <RxDesktop />,
+      title: 'Development',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: <RxReader />,
+      title: 'Copywriting',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: <RxRocket />,
+      title: 'SEO',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+        icon: <RxArrowTopRight />,
+        title: 'SEO',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+  ];
