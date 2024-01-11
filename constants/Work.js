1cd795f79b4d23ec9f460ed5dfe54bf7de63aea4
cbillard <cbillard@com-company.fr>
2024-01-11 16:50:03 +0100

    work page finished

---
+++ constants/Work.js
@@ -0,0 +1,53 @@
+// data
+export const workSlider = {
+    slides: [
+      {
+        images: [
+          {
+            title: 'title1',
+            path: '/assets/images/thumb1.jpg',
+            link: 'https://google.com'
+          },
+          {
+            title: 'title2',
+            path: '/assets/images/thumb2.jpg',
+            link: '2'
+          },
+          {
+            title: 'title3',
+            path: '/assets/images/thumb3.jpg',
+            link: '3'
+          },
+          {
+            title: 'title4',
+            path: '/assets/images/thumb4.jpg',
+            link: '4'
+          },
+        ],
+      },
+      {
+        images: [
+          {
+            title: 'title',
+            path: '/assets/images/thumb4.jpg',
+            link: '4'
+          },
+          {
+            title: 'title',
+            path: '/assets/images/thumb1.jpg',
+            link: '4'
+          },
+          {
+            title: 'title',
+            path: '/assets/images/thumb2.jpg',
+            link: '4'
+          },
+          {
+            title: 'title',
+            path: '/assets/images/thumb3.jpg',
+            link: '4'
+          },
+        ],
+      },
+    ],
+  };
